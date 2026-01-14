8a20efd9cfd229f3e3d745ee0ab6b0c5e67435dd
anindyaaf29 <anindyaaf29@gmail.com>
2026-01-14 16:27:30 +0700

    fixx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ecommerce-uas">
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
